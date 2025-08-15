c03ba6703ea822a5e0a5b44bed947e8d302a5c34
Alk096 <mahamsdoualker@gmail.com>
2025-08-15 15:51:02 +0100

    ensemble debut

---
+++ ensemble.py
@@ -0,0 +1,24 @@
+#creation
+# E = {1,2,3,4}
+# E = set ((1,2,3,4))
+# E = set(rang(0,10,2))
+# E = frozenset((1,2,"En"))
+
+#ajout
+# E.add(element)
+# E.update([1,"En","pop"]) - E.update({1,"En","pop"})
+
+#supprimer
+#E.remove(element) sinon erreur
+#E.disccard(element) sans erreur
+#E.pop() suprimer un element aleatoir
+#E.clear() vider l'ensemble
+
+#operation
+#len(E) nombre d'element
+#sum(E)
+#max(E)
+#min(E)
+
+#duplication
+# E = Eorigine.copy()
